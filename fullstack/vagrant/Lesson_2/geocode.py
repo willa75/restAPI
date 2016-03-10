87f12b5e0071591e133ddb4397248c98e44b6a7c
willa75 <anthonywi99@yahoo.com>
2016-03-09 20:59:49 -0500

    Added starter code for finding restaurant type

--- fullstack/vagrant/Lesson_2/geocode.py
+++ fullstack/vagrant/Lesson_2/geocode.py
@@ -1,12 +1,14 @@
 import httplib2
 import json
 
-def getGetGeocodeLocation(inputString):
+def getGeocodeLocation(inputString):
 	google_api_key = ""
 	locationString = inputString.replace(" ", "+")
 	url = ("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (locationString, google_api_key))
 	h = httplib2.Http()
-	response, content = h.request(url, "GET")
-	result = json.loads(content)
+	result = json.loads(h.request(url,'GET')[1])
 	print "response header: %s \n \n" % response
-	return result
+	#print respsonse
+	latitude = result['results'][0]['geometry']['location']['lat']
+	longitude = result['results'][0]['geometry']['location']['lng']
+	return (latitude, longitude)
